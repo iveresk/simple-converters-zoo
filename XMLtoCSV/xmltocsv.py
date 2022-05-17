38c7e3c5f27ff49102ecedf63fc0335b62e91ada
Ivan Vereshchaga <ivanvereshchaga@Ivans-MacBook-Air.local>
2022-05-17 16:38:14 +0300

    Simple start with a simple XMLtoCSV tool

---
+++ XMLtoCSV/xmltocsv.py
@@ -0,0 +1,24 @@
+import xmltodict
+import pandas as pd
+import argparse
+
+
+def main(xmlfile, csvfile):
+    try:
+        f = open(xmlfile, "r")
+        xml = f.read()
+    except Exception:
+        print("\n Error openning XML file: " + str(Exception))
+    df = pd.DataFrame(xmltodict.parse(str(xml)))
+    df.rename(columns=lambda x: x.replace('@', ''), inplace=True)
+    df.to_csv(csvfile)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type=argparse.FileType(), nargs='+')
+    args = parser.parse_args()
+    if args is None:
+        print("\n To launch the code you should stick to the next format:")
+        print("python3 xmltocsv.py <xml_filename> <csv_filename>")
+    main(args.file[0], args.file[1])
