3041e1ee6a2080b483e05e636c81feba7a10a0ad
iveresk <iveresk@users.noreply.github.com>
2022-06-04 12:22:11 +0300

    made the script to search by param

--- nmap_search/nmap-iis-searcher.sh
+++ nmap_search/nmap-iis-searcher.sh
@@ -2,25 +2,30 @@
 
 REGIME=$1
 IP=$2
-PORT=$3
+SEARCH="${3}"
+PORT=$4
 
 # Checking if we have a target to attack
 if [ -z "$IP" ];then
 	REGIME="-h"
 fi
 
+if [ -z "$SEARCH" ];then
+        SEARCH="IIS"
+fi
+
 # Small standart how-to.
 if [ -z "$REGIME" ] || [ "$REGIME" == "-h" ] || [ $REGIME == "--help" ];then
-	echo "-----------------Welcome-to-nmap-iis-searcher-by-1veresk---------------+";
-	echo "+----------------------------------------------------------------------+";
-	echo "+-------------------For-The-Help---------------------------------------+";
-	echo "Example#1: ./nmap-iis-searcher.sh -h-----------------------------------+";
-	echo "Example#2: ./nmap-iis-searcher.sh --help-------------------------------+";
-	echo "+-------------------For-The-URL-Check----------------------------------+";
-	echo "Example#1: ./nmap-iis-searcher.sh -u <IP> <PORT> [Default PORT = 80---]+";
-	echo "+-------------------For-The-File-Check---------------------------------+";
-	echo "Example#1: ./nmap-iis-searcher.sh -f <FILENAME>------------------------+";
-	echo "+----------------------------------------------------------------------+";
+	echo "-------------------------Welcome-to-nmap-iis-searcher-by-1veresk--------------------------------+";
+	echo "+-----------------------------------------------------------------------------------------------+";
+	echo "+-------------------For-The-Help----------------------------------------------------------------+";
+	echo "Example#1: ./nmap-iis-searcher.sh -h------------------------------------------------------------+";
+	echo "Example#2: ./nmap-iis-searcher.sh --help--------------------------------------------------------+";
+	echo "+-------------------For-The-URL-Check-----------------------------------------------------------+";
+	echo "Example#1: ./nmap-iis-searcher.sh -u <IP> <SEARCH TEXT> <PORT> [Default PORT = 80, SEARCH="IIS"]+";
+	echo "+-------------------For-The-File-Check----------------------------------------------------------+";
+	echo "Example#1: ./nmap-iis-searcher.sh -f <FILENAME> IIS 80------------------------------------------+";
+	echo "+-----------------------------------------------------------------------------------------------+";
 	exit 1;
 fi
 
@@ -31,9 +36,9 @@ fi
 
 # Scanning Target
 if [ -e "$IP" ];then
-	nmap -T5 -p $PORT -iL $IP --script http-grep --script-args='match="IIS"' > "output.txt"
+	nmap -T5 -p $PORT -iL $IP --script http-grep --script-args match=$SEARCH > "nmap_output.txt"
 else
-	nmap -T5 -p $PORT $IP --script http-grep --script-args='match="IIS"' > "output.txt"
+	nmap -T5 -p $PORT $IP --script http-grep --script-args match=$SEARCH > "nmap_output.txt"
 fi
 
-cat output.txt | egrep -o "\(1\) http://[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | egrep -o "([0-9]{1,3}\.){3}[0-9]{1,3}" > "targets.txt"
+cat nmap_output.txt | egrep -o "\(1\) http://[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | egrep -o "([0-9]{1,3}\.){3}[0-9]{1,3}" > "targets.txt"
